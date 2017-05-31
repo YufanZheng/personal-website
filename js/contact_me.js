44e2727a19aefa0abc153eca2b492c74cdc8c302
Yufan Zheng <yufan.zheng1@gmail.com>
2017-05-31 09:57:39 +0200

    add personal presentation

--- js/contact_me.js
+++ js/contact_me.js
@@ -37,7 +37,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your message has been sent. I will reply to you as soon as possible. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
